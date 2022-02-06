f719dfde31db000d52a3a5afc9b2a98302e76a30
AlexxalexS <37020080+AlexxalexS@users.noreply.github.com>
2022-02-06 17:35:32 +0300

    feat: routes

--- server.js
+++ server.js
@@ -39,8 +39,13 @@ app.get("/", (req, res) => {
     res.json({message: "Server is running"});
 });
 
+// routes
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
+
+
 // set port, listen for requests
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} http://localhost:${PORT}`);
 });
